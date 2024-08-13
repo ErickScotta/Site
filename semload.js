97e3cf9df51a0e8fdda54486f25aaa3970eed7cb
Erick Scotta Turcatto <125623284+ErickScotta@users.noreply.github.com>
2024-08-12 21:22:36 -0300

    Add files via upload
    certoooo

---
+++ semload.js
@@ -0,0 +1,30 @@
+function refresh(conteudo){
+    const larguraJanela = window.innerWidth;
+    if(larguraJanela<=480){
+        var url = 'p.html'
+    }else if(larguraJanela > 480 && larguraJanela <= 767){
+        var url = 'm.html'
+    }else if(larguraJanela >= 768 && larguraJanela <= 1024){
+        var url = 'g.html'
+    }else if(larguraJanela >= 1025 && larguraJanela <=1280){
+        var url = 'gg.html'
+    }else{//talvez (largurajanela >= 1280)
+        var url = 'extrag.html'
+    }
+    
+    
+    var target = conteudo
+    var xml = new XMLHttpRequest()
+
+    xml.onreadystatechange = function () {
+        if (xml.readyState == 4 && xml.status == 200 ) {
+            document.getElementById(target).innerHTML = xml.responseText
+        }
+    }
+
+    xml.open("GET", url, true)
+
+    xml.send()
+}
+
+
