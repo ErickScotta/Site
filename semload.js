bd05560ccaa471638421fda19fa6a4a406e65bee
Erick Scotta Turcatto <125623284+ErickScotta@users.noreply.github.com>
2024-08-13 00:07:36 -0300

    Update semload.js
    se der merda e so voltar no ultimo se der merda

--- semload.js
+++ semload.js
@@ -1,8 +1,10 @@
 function refresh(conteudo){
     const larguraJanela = window.innerWidth;
-    if(larguraJanela<=480){
+    if(larguraJanela<=389){
         var url = 'p.html'
-    }else if(larguraJanela > 480 && larguraJanela <= 767){
+    }else if(larguraJanela >=390 && larguraJanela <= 600){
+        var url = 'pm.html'
+    }else if(larguraJanela >= 601 && larguraJanela <= 767){
         var url = 'm.html'
     }else if(larguraJanela >= 768 && larguraJanela <= 1024){
         var url = 'g.html'
